c7f5d69c2225bb399705020158cd30c13b71cc7b
= <=>
2018-10-08 09:13:48 +0700

    fixed JS

--- Task JavaScript/Ex-1/js/action.js
+++ Task JavaScript/Ex-1/js/action.js
@@ -1,21 +1,31 @@
 $(document).ready(function () {
-	var str      = $('#txt-name-js');
-	var btnAdd   = $('#btn-add');
+	var str      = $('#js-txt-name');
+	var btnAdd   = $('#js-btn-add');
 	var list  	 = $('#list');
+	var maxLength= 30;
 
 	btnAdd.click(function () {
 		var name = str.val().trim();
+
 		if (name === '') {
-			alert('Bạn chưa nhập tên sản phẩm.');
+			alert('The name of product must not be empty.');
+			str.focus();
 			return false;
 		}
 
-		list.append('<li class="item"><span>' + ' </span>' + name + '<button id="btn-remove"> X</button></li>');
+		if (name.length > maxLength) { 
+			alert('The string length must be less than '+ maxLength + '.');
+			str.val('');
+			str.focus();
+            return false;
+		}
+		
+		list.append('<li class="item"><span>' + ' </span>' + name + '<button id="js-btn-remove"> X</button></li>');
 		str.val('');
+		str.focus();
 	});
 
-
-	$(document).on('click', '#btn-remove', function () {
+	$(document).on('click', '#js-btn-remove', function () {
 		$(this).parent().remove();
-	})
-})
+	});
+});

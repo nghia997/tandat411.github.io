570239ca6a2aebf916162833cf1cad132fe3d663
= <=>
2018-10-09 18:07:07 +0700

    Bài tập JS - đề 3

--- Task JavaScript/Ex-3/js/action.js
+++ Task JavaScript/Ex-3/js/action.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-	
+	var btnPrev   = $('#btn-prev');
+	var btnNext   = $('#btn-next');
+	var listItems = $('.js-list-item');
+	var listThumbs= $('.js-list-thumb');
+
+	btnPrev.click(function () {
+		listItems.children('li:last').animate({width : '650px'}).prependTo(listItems);
+	});
+
+	btnNext.click(function () {
+		listItems.children('li:first').animate({width : '0px'}).appendTo(listItems);
+	});
+
+	$(document).on('click', '.js-list-thumb li', function () {
+		var pos = $(this).index();
+		listItems.children().hide();
+		//listItems.children()[pos].show();
+		listThumbs.children()[pos].css('opacity', '0.5');
+	});
 });

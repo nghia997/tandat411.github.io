d27197d303e65215d2188c80cae92a27f4317ff8
= <=>
2018-10-09 13:33:20 +0700

    Bài tập JS - 2 fix Responsive

--- Task JavaScript/Ex-2/js/action.js
+++ Task JavaScript/Ex-2/js/action.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 		element.css('pointer-events', 'none');
 		setTimeout(function () {
 			element.css('pointer-events', 'auto');
-		}, 500);
+		}, 300);
 	}
 
 	// Set animate the popup when user click on image off

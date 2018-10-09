66881fe7c67ae401d9cbd5789eab80411e7aab01
= <=>
2018-10-09 22:36:12 +0700

    Bài tập JS - đề 3

--- Task JavaScript/Ex-3/js/action.js
+++ Task JavaScript/Ex-3/js/action.js
@@ -1,21 +1,38 @@
 $(document).ready(function () {
 	var btnPrev   = $('#btn-prev');
 	var btnNext   = $('#btn-next');
-	var listItems = $('.js-list-item');
-	var listThumbs= $('.js-list-thumb');
+	var listItems = $('#js-list-item');
+	var listThumbs= $('#js-list-thumb');
+	var maxLength = listItems.children().length;
+	var count     = 0;
+
+	listThumbs.children('li:first').css('opacity', '0.5');
+
+	function setOpacity(position) {
+		listThumbs.children().css('opacity', '1');
+		listThumbs.children().eq(position).css('opacity', '0.5');
+	}
 
 	btnPrev.click(function () {
-		listItems.children('li:last').animate({width : '650px'}).prependTo(listItems);
+		count -= 1;
+		if(count < 0) {count = maxLength - 1;}
+		listItems.children().hide();
+		listItems.children().eq(count).show();
+		setOpacity(count);
 	});
 
 	btnNext.click(function () {
-		listItems.children('li:first').animate({width : '0px'}).appendTo(listItems);
+		count += 1;
+		if(count > 4) {count = 0;}
+		listItems.children().hide();
+		listItems.children().eq(count).show();
+		setOpacity(count);
 	});
 
-	$(document).on('click', '.js-list-thumb li', function () {
+	$(document).on('click', '#js-list-thumb li', function () {
 		var pos = $(this).index();
+		setOpacity(pos);
 		listItems.children().hide();
-		//listItems.children()[pos].show();
-		listThumbs.children()[pos].css('opacity', '0.5');
+		listItems.children().eq(pos).show();
 	});
 });

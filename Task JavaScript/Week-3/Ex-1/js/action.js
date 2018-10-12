88ab7661ad6e1deea2531db0d14d642e473a0f1d
= <=>
2018-10-12 18:04:20 +0700

    Bài tập JS 1 - Tuần 3

---
+++ Task JavaScript/Week-3/Ex-1/js/action.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	var canvas = $('#js-canvas');
+	var context = canvas[0].getContext('2d');
+	var pi = Math.PI;
+
+	
+	
+})
---
+++ Task JavaScript/Week-3/Ex-1/js/data.js
@@ -0,0 +1,18 @@
+// All variables
+	var text = {
+		success: 'ĐÃ ĐẠT', // Success text
+		fail: 'CHƯA ĐẠT',  // Fail text
+		title: "BIỂU ĐỒ TỔNG QUÁT KHUNG NĂNG LỰC",
+		font: "20px Arial", // set font-style for text
+		color: "gray" // color of text
+	}
+	var data = {
+		success: 0.8,   // data of text success
+		fail: 0.2   // data of text fail
+	};
+
+	var color = {
+		blue: "#009ed5",
+		darkBlue: "#456aa4",
+		red: "#e4322b"
+	};

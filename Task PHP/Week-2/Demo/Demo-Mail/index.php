b416c80acfd8d6c00a574e41bf596a52c267a9b3
= <=>
2018-11-03 00:00:18 +0700

    Demo mail

--- Task PHP/Week-2/Demo/Demo-Mail/index.php
+++ Task PHP/Week-2/Demo/Demo-Mail/index.php
@@ -1,16 +1,12 @@
 <?php
-$recipient = 'nhocshockuteo@gmail.com';
-$headers = 'From: 1551010024.dat@gmail.com';
-$headers .="MIME-Version: 1.0\r\nContent-Type: text/html; charset=iso-8859-1";
-$subject = 'Hello World';
-$message = '<html><body><h1>This is a test</h1></body></html>';
-$retval = mb_send_mail($recipient, $subject, $message, $headers);
+$to = 'nhocshockuteo@gmail.com';
+$headers = "From: 1551010024.dat@gmail.com \r\n";
+$subject = 'Phần tiêu đề';
+$message = "あるひ、おばあさんはなくなりました。おばあさんをおはかにほうむってから、ゆきはなけなしのかざいをあつめ、みやこへむけてしゅっぱつしました。";
 
-     if( $retval == true )
-     {
-         echo "Gửi email thành công ...";
-     }
-     else
-     {
-         echo "Không thể gửi email ...";
-     }
+ if (mb_send_mail($to, $message, $subject, $headers)) {
+     echo "Gửi email thành công ...";
+ } else {
+     echo "Không thể gửi email ...";
+ }
+ 
